e29fe100cc7b1ba650159d33391c6ab7227726f1
Vedant Daigavane <vedant.daigavane@rksv.in>
2023-01-05 23:14:19 +0530

    code formatted and request mapping added for testing

--- server/src/main/java/com/sprint/server/controller/TestController.java
+++ server/src/main/java/com/sprint/server/controller/TestController.java
@@ -7,9 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RequestMapping("/test")
 @RestController
 public class TestController {
 
